7e982cf3a26478ad3724cc03ae25520c77a92149
Andrew <122319655+adavidschmidt@users.noreply.github.com>
2026-01-24 13:35:29 -0500

    Add basic server functionality with additional handler to handle tracking hits, health, and reset hits

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func main() {
+	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerFileserverHits)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	log.Fatal(s.ListenAndServe())
+}
+
+func (cfg *apiConfig) handlerFileserverHits(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
